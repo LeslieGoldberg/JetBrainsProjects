b4838db3c9372606d69b996c81e96a71bf405cd5
LeslieGoldberg <67349768+LeslieGoldberg@users.noreply.github.com>
2020-08-04 16:44:45 -0500

    Add files via upload
    Completed project.

--- browser.py
+++ browser.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import requests
+from bs4 import BeautifulSoup
+from colorama import Fore
 
 
 class Browser:
@@ -15,11 +17,29 @@ class Browser:
     def new_site(self, website):
         nick_name = website.lstrip('https://').rsplit('.', 1)[0]
         path_now = os.path.join(path, nick_name) + '.txt'
-        r = requests.get(website)
+        useful_text = self.parse_site(website)
         with open(path_now, 'w') as saving_site:
-            saving_site.write(r.text)
+            saving_site.write(useful_text)
         self.tabs_library[nick_name] = path_now
         self.known_site(website, path_now)
+
+    def is_only_string_in_tag(self, s):
+        """Return True if this string is the only child in the parent tag"""
+        tag_list = ['p', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ul', 'ol', 'li']
+        return s == s.parent.string and s.parent.name in tag_list
+
+    def parse_site(self, website):
+        r = requests.get(website)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        text_list = soup.find_all(string=self.is_only_string_in_tag)
+        for i, string in enumerate(text_list):
+            for link in soup.find_all('a'):
+                if string in link:
+                    blue_string = Fore.BLUE + string
+                    text_list.pop(i)
+                    text_list.insert(i, blue_string)
+        text = '\n'.join(text_list)
+        return text
 
     def known_site(self, site, path_to_file):
         self.tabs_history.append(site)
